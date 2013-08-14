2c89861cbfd9190dbffc3e3328fe9d222ad00191
santi8ago8 <santi8ago8@gmail.com>
2013-08-14 19:53:58 -0300

    don't work jquery node for windows :(

---
+++ saveMensajes.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: SantiagoPC
+ * Date: 14/08/13
+ * Time: 19:29
+ * To change this template use File | Settings | File Templates.
+ */
+
+var fs = require("fs");
+
+
+fs.readFile('fb_data/html/messages.htm', function (err, data) {
+
+    var $ = require('jquery').create();
+
+});
